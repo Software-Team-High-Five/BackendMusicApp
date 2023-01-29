355224c49d63806317ba559e321cb6903b67d4a0
LucasEwald <42420052+LucasEwald@users.noreply.github.com>
2023-01-29 16:47:02 -0600

    fix typo in song.model.js

--- app/models/song.model.js
+++ app/models/song.model.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, Sequelize) => {
     }
     ,translation: {
       type: Sequelize.STRING(750)
-      ,allowNull: TRUE
+      ,allowNull: true
     }
   });
   return User;
